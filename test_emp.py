de725272c2e035ef7627203c0caee7a5cd080059
raigon44 <raigonka@gmail.com>
2018-05-20 18:30:19 +0530

    new test methods

--- test_emp.py
+++ test_emp.py
@@ -5,26 +5,30 @@ from emp import Employee
 
 class Testemp(unittest.TestCase):
     
+    def setUp(self):
+        self.empt1 = Employee('TESTER1','LA',13,300,41)
+        self.empt2 = Employee('TESTER2','LA',13,0,32)
+        
+    def tearDown(self):
+        print("tear down")
     
     
     def test_yearlysalary(self):
-        empt1 = Employee('TESTER1','LA',13,300,41)
-        self.assertEqual(empt1.yearly_salary(),3600)
-        empt2 = Employee('TESTER1','LA',13,0,32)
+        
+        self.assertEqual(self.empt1.yearly_salary(),3600)
+        
         with self.assertRaises(ValueError): #context manager: can be used for testing exceptions
-            empt2.yearly_salary()
+            self.empt2.yearly_salary()
         
     def test_fullname(self):
-        empt1 = Employee('TESTER1','LA',13,300,41)
-        empt2 = Employee('TESTER2','LA',13,2000,32)
-        self.assertEqual(empt1.full_name, 'TESTER1 LA')
-        self.assertEqual(empt2.full_name, 'TESTER2 LA')
+
+        self.assertEqual(self.empt1.full_name, 'TESTER1 LA')
+        self.assertEqual(self.empt2.full_name, 'TESTER2 LA')
         
     def test_email(self):
-        empt1 = Employee('TESTER1','LA',13,300,41)
-        empt2 = Employee('TESTER2','LA',13,2000,32)
-        self.assertEqual(empt1.email_add, 'TESTER1LA@sap.com')
-        self.assertEqual(empt2.email_add, 'TESTER2LA@sap.com')
+
+        self.assertEqual(self.empt1.email_add, 'TESTER1LA@sap.com')
+        self.assertEqual(self.empt2.email_add, 'TESTER2LA@sap.com')
         
         
 
